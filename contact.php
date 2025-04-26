d1ed54531ba7b8fb4bcefa5ffde080a9b203a991
salma <salmalaouy83@gmail.com>
2025-04-26 20:42:32 +0100

    php dyal contact

--- contact.php
+++ contact.php
@@ -2,9 +2,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "root";
+$pass = "";
 $dbname = "cabinet_medical";
-
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connexion échouée : " . $conn->connect_error);
