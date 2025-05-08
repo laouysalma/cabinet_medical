634412c60ef9471d781b4a54f280bc2a61f768f2
laouysalma <salmalaouy83@gmail.com>
2025-05-08 12:15:11 +0100

    Update backup.sql

--- backup.sql
+++ backup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 25 avr. 2025 à 12:22
+-- Généré le : jeu. 08 mai 2025 à 13:14
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -34,6 +34,21 @@ CREATE TABLE `contact` (
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`name`, `email`, `subject`, `message`) VALUES
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('LAOUY SALMA', 'salmalaouy83@gmail.com', 'demande de certificat medical', 'dkkks'),
+('hind', 'salmalaouy83@gmail.com', 'demande de certificat medical', 'demande de certificat medical'),
+('LAOUY SALMA', 'salmalaouy83@gmail.com', 'demande de certificat medical', 'ssasasaajjjjjaa'),
+('lwalida', 'salmalaouy83@gmail.com', 'demande de certificat medical', 'ajjaja'),
+('LAOUY SALMA', 'salmalaouy83@gmail.com', 'demande de certificat medical', 'jcdjsJ');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +68,58 @@ CREATE TABLE `rdv` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Déchargement des données de la table `rdv`
+--
+
+INSERT INTO `rdv` (`id`, `full_name`, `email`, `phone`, `rdv_date`, `rdv_time`, `doctor`, `created_at`, `status`) VALUES
+(1, 'LAOUY SALMA', 'salmalaouy83@gmail.com', '0645362198', '0000-00-00', '00:00:00', 'Dr Nadia Ben Salah (Pédiatre)', '0000-00-00 00:00:00', 'pending'),
+(2, 'LAOUY SALMA', 'salmalaouy83@gmail.com', '0645362198', '0000-00-00', '00:00:00', 'Dr Nadia Ben Salah (Pédiatre)', '0000-00-00 00:00:00', 'pending'),
+(3, 'saluuu', 'salmalaouy83@gmail.com', '0645362198', '0000-00-00', '00:00:00', 'Dr Nadia Ben Salah (Pédiatre)', '0000-00-00 00:00:00', 'pending'),
+(4, 'sali ', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(5, 'sali ', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(6, 'sali ', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(7, 'sali ', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(8, 'LAOUY ASMA', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(9, 'LAOUY ASMA', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(10, 'sali ', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(11, 'sali ', 'aishaasma2004@gmail.com', '0712415668', '0000-00-00', '00:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(12, 'hiba ouiraouane', 'hibaouiraouane@gmail.com', '0600000000', '0033-02-12', '16:00:00', 'Dr Nadia Ben Salah (Pédiatre)', '0000-00-00 00:00:00', 'pending'),
+(13, 'fatima_ezzahra', 'fatima_ezzahra@gmail.com', '06000000000000', '0000-00-00', '14:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(14, 'hafsa ', 'hafsa@gmail.com', '0606070707', '2026-01-11', '15:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(15, 'LAOUY SALMA', 'salmalaouy83@gmail.com', '0645362198', '0577-04-04', '15:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(16, 'LAOUY SALMA', 'salmalaouy83@gmail.com', '0645362198', '0577-04-04', '15:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending'),
+(17, 'LAOUY SALMA', 'salmalaouy83@gmail.com', '0645362198', '2025-05-22', '16:00:00', 'Dr Youssef Mahdi (Généraliste)', '0000-00-00 00:00:00', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rendezvous`
+--
+
+CREATE TABLE `rendezvous` (
+  `id` int(11) NOT NULL,
+  `id_patient` int(11) DEFAULT NULL,
+  `id_medecin` int(11) DEFAULT NULL,
+  `date_rdv` datetime DEFAULT NULL,
+  `sujet` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mot_de_passe` varchar(255) DEFAULT NULL,
+  `role` enum('patient','medecin','admin') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -63,6 +130,21 @@ ALTER TABLE `rdv`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_patient` (`id_patient`),
+  ADD KEY `id_medecin` (`id_medecin`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -70,7 +152,30 @@ ALTER TABLE `rdv`
 -- AUTO_INCREMENT pour la table `rdv`
 --
 ALTER TABLE `rdv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
+  ADD CONSTRAINT `rendezvous_ibfk_1` FOREIGN KEY (`id_patient`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `rendezvous_ibfk_2` FOREIGN KEY (`id_medecin`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
